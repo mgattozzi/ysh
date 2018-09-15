3cc267758602a20d27311df808a87d15ce5ea3f3
Eliza Weisman <eliza@buoyant.io>
2018-09-15 13:09:29 -0700

    wip: Prototype TrieMap::insert impls + bench

---
+++ trie/benches/bench.rs
@@ -0,0 +1,55 @@
+#![feature(test)]
+use ysh_trie::TrieMap;
+use test::Bencher;
+
+use std::collections::HashMap;
+
+fn keys() -> Vec<String> {
+    let mut keys = Vec::new();
+    for a in 1..=26 {
+        for b in 1..=26 {
+            for c in 1..=26 {
+                for d in 1..=26 {
+                    keys.push(format!("{}{}{}{}", a, b, c, d));
+                }
+            }
+        }
+    }
+    keys
+}
+
+#[bench]
+fn bench_insert(b: &mut Bencher) {
+    let keys = keys();
+    b.iter(|| {
+        let mut trie: TrieMap<char, ()> = TrieMap::new();
+        for key in &keys {
+            trie.insert(key.chars(), ());
+        }
+        trie
+    });
+}
+
+#[bench]
+fn bench_insert_recursive(b: &mut Bencher) {
+    let keys = keys();
+    b.iter(|| {
+        let mut trie: TrieMap<char, ()> = TrieMap::new();
+        for key in &keys {
+            trie.insert_recursive(key.chars(), ());
+        }
+        trie
+    });
+}
+
+#[bench]
+fn bench_insert_hashmap(b: &mut Bencher) {
+    let keys = keys();
+    b.iter(|| {
+        let mut map = HashMap::new();
+        for key in &keys {
+            map.insert(key, ());
+        }
+        map
+    });
+}
--- trie/src/lib.rs
+++ trie/src/lib.rs
@@ -16,7 +16,7 @@ where
 }
 
 #[derive(Debug, Default)]
-struct Node<K, V, S>
+struct Node<K, V, S = RandomState>
 where
     K: Hash + Eq,
     S: hash::BuildHasher,
@@ -25,38 +25,110 @@ where
     value: Option<V>,
 }
 
+impl<K, V> TrieMap<K, V>
+where
+    K: Hash + Eq,
+{
+    pub fn new() -> Self {
+        Self {
+            root: Node::new(),
+        }
+    }
+}
+
 impl<K, S, V> TrieMap<K, V, S>
 where
     K: Hash + Eq,
     S: hash::BuildHasher + Clone,
 {
-    pub fn insert<'q, I, Q: 'q + ?Sized>(&mut self, key: I, value: V) -> Option<V>
+    pub fn insert<I>(&mut self, key: I, value: V) -> Option<V>
     where
-        I: IntoIterator<Item = &'q Q>,
-        Q: ToOwned<Owned = K>,
-        K: Borrow<Q>,
+        I: IntoIterator<Item = K>,
     {
         let previous = key.into_iter().fold(&mut self.root, |node, frag| {
             let hasher = node.children.hasher().clone();
             node.children
-                .entry(frag.to_owned())
-                .or_insert_with(|| Node::new(hasher))
-        })
-        .value;
-        mem::replace(previous, Some(value))
+                .entry(frag)
+                .or_insert_with(|| Node::with_hasher(hasher))
+        });
+        mem::replace(&mut previous.value, Some(value))
+    }
+
+    // #[cfg(test)]
+    pub fn insert_recursive<I>(&mut self, key: I, value: V) -> Option<V>
+    where
+        I: IntoIterator<Item = K>,
+    {
+        self.root.continue_inserting(key.into_iter(), value)
+    }
+}
+
+impl<K, V> Node<K, V>
+where
+    K: Hash + Eq,
+{
+    fn new() -> Self {
+        Self {
+            children: HashMap::new(),
+            value: None,
+        }
     }
 }
 
 impl<K, S, V> Node<K, V, S>
 where
     K: Hash + Eq,
-    S: hash::BuildHasher,
+    S: hash::BuildHasher + Clone,
 {
-
-    fn new(state: S) -> Self {
+    fn with_hasher(state: S) -> Self {
         Self {
             children: HashMap::with_hasher(state),
             value: None,
+        }
+    }
+
+    // #[cfg(test)]
+    #[inline]
+    fn continue_inserting<I>(&mut self, mut key: I, value: V) -> Option<V>
+    where
+        I: Iterator<Item = K>,
+    {
+        match key.next() {
+            None => mem::replace(&mut self.value, Some(value)),
+            Some(frag) => {
+                let hasher = self.children.hasher().clone();
+                self.children
+                    .entry(frag)
+                    .or_insert_with(|| Node::with_hasher(hasher))
+                    .continue_inserting(key, value)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn keys() -> Vec<String> {
+        let mut keys = Vec::new();
+        for a in 1..=26 {
+            for b in 1..=26 {
+                for c in 1..=26 {
+                    for d in 1..=26 {
+                        keys.push(format!("{}{}{}{}", a, b, c, d));
+                    }
+                }
+            }
+        }
+        keys
+    }
+
+    #[test]
+    fn insert() {
+        let mut trie: TrieMap<char, ()> = TrieMap::new();
+        for key in &keys() {
+            trie.insert(key.chars(), ());
         }
     }
 }
